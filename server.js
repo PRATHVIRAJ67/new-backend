acc0c46efb9040bbb4088540aa0603221e009fbe
PRATHVIRAJ67 <104517214+PRATHVIRAJ67@users.noreply.github.com>
2024-11-15 11:41:41 +0530

    first

--- server.js
+++ server.js
@@ -2,5 +2,5 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
